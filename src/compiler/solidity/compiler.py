3074f6e0cef7153d53426c959415a6c119750af0
Benjamin Bichsel <benjamin.bichsel@infk.ethz.ch>
2019-08-31 15:22:02 +0200

    bugfix

--- src/compiler/solidity/compiler.py
+++ src/compiler/solidity/compiler.py
@@ -8,17 +8,19 @@ solc = 'solcjs'
 
 
 def compile_solidity_code(code: str, output_directory: str):
+	if not os.path.exists(output_directory):
+		os.makedirs(output_directory)
 
-    source_name = 'code.sol'
-    file_path = os.path.join(output_directory, source_name)
-    with open(file_path, "w") as f:
-        f.write(code)
+	source_name = 'code.sol'
+	file_path = os.path.join(output_directory, source_name)
+	with open(file_path, "w") as f:
+		f.write(code)
 
-    return compile_solidity(output_directory, source_name)
+	return compile_solidity(output_directory, source_name)
 
 
 def compile_solidity(path: str, source_file: str, output_directory: str = None):
-    if not output_directory:
-        output_directory = path
-    output_directory = os.path.abspath(output_directory)
-    return run_command([solc, '--bin', '--overwrite',  '-o', output_directory, source_file], path)
+	if not output_directory:
+		output_directory = path
+	output_directory = os.path.abspath(output_directory)
+	return run_command([solc, '--bin', '--overwrite',  '-o', output_directory, source_file], path)
