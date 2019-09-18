d826d020157c34bf5ca9d7f59f69b2d881e20c18
Samuel Steffen <samuel.steffen@inf.ethz.ch>
2019-09-18 14:24:13 +0200

    rm old eval files

--- eval-ccs2019/old/optimization-example/truffle-app/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var Test = artifacts.require("Test");
-var Verifier = artifacts.require("Verifier");
-
-module.exports = function(deployer) {
-    deployer.deploy(Verifier).then(function() {
-        return deployer.deploy(Test, Verifier.address);
-    });
-};
--- eval-ccs2019/old/truffle_welfare/migrations/2_welfare.js
+++
@@ -1,9 +0,0 @@
-var Welfare = artifacts.require("Welfare");
-var EligibilityChecker = artifacts.require("EligibilityChecker");
-
-module.exports = function(deployer) {
-    deployer.deploy(EligibilityChecker).then(function() {
-        var taxAuthority = web3.eth.accounts[0];
-        return deployer.deploy(Welfare, taxAuthority, EligibilityChecker.address);
-    });
-};
