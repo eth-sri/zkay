16d76724a2c91305847896d1351285d687dbcf5f
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-10-03 10:23:48 +0200

    add colored print context

--- src/utils/progress_printer.py
+++ src/utils/progress_printer.py
@@ -1,4 +1,5 @@
 import contextlib
+from enum import Enum
 
 
 @contextlib.contextmanager
@@ -6,3 +7,21 @@ def print_step(name):
 	print(f'{name}... ', end='', flush=True)
 	yield
 	print('done')
+
+
+class TermColor(Enum):
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
+
+@contextlib.contextmanager
+def colored_print(color: TermColor):
+	print(color.value, end='')
+	yield
+	print(TermColor.ENDC.value, end='')
