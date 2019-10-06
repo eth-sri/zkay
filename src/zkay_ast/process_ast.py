9c37c03bb3c5587b415347e12c474e06c84270e1
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-10-06 16:33:13 +0200

    do solc type check before zkay type check

--- src/zkay_ast/process_ast.py
+++ src/zkay_ast/process_ast.py
@@ -43,14 +43,14 @@ def get_processed_ast(code, parents=True, link_identifiers=True, check_return=Tr
 				print(f'{str(e)}\n')
 			raise ParseExeception()
 
-	# Zkay preprocessing and type checking
-	process_ast(ast, parents, link_identifiers, check_return, alias_analysis, type_check)
-
 	# Solc type checking
 	with print_step("Running through solc"):
 		from compiler.solidity.fake_solidity_compiler import fake_solidity_code
 		fake_code = fake_solidity_code(str(code))
 		check_solc_errors(code, fake_code)
+
+	# Zkay preprocessing and type checking
+	process_ast(ast, parents, link_identifiers, check_return, alias_analysis, type_check)
 
 	return ast
 
