a13efc7e1b5181cd606bc96f4a9a310681a19bb7
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-10-26 18:07:59 +0200

    generate manifest file with output metadata

---
+++ zkay/compiler/privacy/manifest.py
@@ -0,0 +1,8 @@
+class Manifest:
+    project_dir = 'project-dir'
+    uuid = 'uuid'
+    contract_filename = 'contract-file'
+    proving_scheme = 'proving-scheme'
+    pki_lib = 'pki-lib'
+    verify_lib = 'verify-lib'
+    verifier_names = 'verifier-names'
--- zkay/compiler/privacy/zkay_frontend.py
+++ zkay/compiler/privacy/zkay_frontend.py
@@ -1,11 +1,14 @@
+import json
 import os
 from copy import deepcopy
 import shutil
 import tempfile
 import pathlib
+import uuid
 
 from zkay.compiler.privacy.circuit_generation.backends.zokrates_generator import ZokratesGenerator
 from zkay.compiler.privacy.circuit_generation.circuit_generator import CircuitGenerator
+from zkay.compiler.privacy.manifest import Manifest
 from zkay.compiler.privacy.proving_schemes.gm17 import ProvingSchemeGm17
 from zkay.compiler.privacy.proving_schemes.proving_scheme import ProvingScheme
 from zkay.compiler.privacy.transformer.zkay_transformer import transform_ast, pki_contract_name
@@ -30,8 +33,25 @@ def compile_zkay(ast: AST, output_dir: str, filename: str, get_binaries: bool = 
         with open(os.path.join(output_dir, ProvingScheme.verify_libs_contract_filename), 'w') as f:
             f.write(ProvingScheme.get_library_code())
 
+    ps = ProvingSchemeGm17()
+    cg = ZokratesGenerator(ast, list(zkt.circuit_generators.values()), ps, output_dir)
+
+    # Generate manifest
+    manifest = {
+        Manifest.uuid: uuid.uuid1().hex,
+        Manifest.contract_filename: filename,
+        Manifest.proving_scheme: ps.name,
+        Manifest.pki_lib: f'{pki_contract_name}.sol',
+        Manifest.verify_lib: ProvingScheme.verify_libs_contract_filename,
+        Manifest.verifier_names: {
+            f'{cc.fct.parent.idf.name}.{cc.fct.name}': cc.verifier_contract.contract_type.type_name.names[0].name for cc in
+            cg.circuits_to_prove
+        }
+    }
+    with open(os.path.join(output_dir, 'manifest.json'), 'w') as f:
+        f.write(json.dumps(manifest))
+
     # Generate circuits and corresponding verification contracts
-    cg = ZokratesGenerator(ast, list(zkt.circuit_generators.values()), ProvingSchemeGm17(), output_dir)
     cg.generate_circuits(import_keys=False)
 
     if get_binaries:
@@ -48,13 +68,15 @@ def package_zkay(zkay_input_filename: str, cg: CircuitGenerator):
         # create archive with zkay code + all verification and prover keys
         root = pathlib.Path(cg.output_dir)
         infile = pathlib.Path(zkay_input_filename)
+        manifestfile = root.joinpath("manifest.json")
         filenames = [pathlib.Path(p) for p in cg.get_all_key_paths()]
-        for p in filenames + [infile]:
+        for p in filenames + [infile, manifestfile]:
             if not p.exists():
                 raise FileNotFoundError()
 
         tmpdir = tempfile.mkdtemp()
         shutil.copyfile(infile, os.path.join(tmpdir, infile.name))
+        shutil.copyfile(manifestfile, os.path.join(tmpdir, manifestfile.name))
         for p in filenames:
             pdir = os.path.join(tmpdir, p.relative_to(root).parent)
             if not os.path.exists(pdir):
