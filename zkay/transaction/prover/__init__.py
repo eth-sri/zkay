b0c1ac65dbf29961d2ac941701374994a430ab13
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-10-26 19:51:51 +0200

    implement zokrates prover backend

---
+++ zkay/transaction/prover/__init__.py
@@ -0,0 +1,1 @@
+from .zokrates import ZokratesProver
---
+++ zkay/transaction/prover/zokrates.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from zkay.compiler.zokrates.compiler import generate_proof
+from zkay.transaction.interface import ZkayProverInterface
+
+
+class ZokratesProver(ZkayProverInterface):
+    def _generate_proof(self, verifier_dir: str, priv_values: List[int], in_vals: List[int], out_vals: List[int]) -> List[int]:
+        args = list(map(int, priv_values + in_vals + out_vals))
+        proof_data = generate_proof(f'{verifier_dir}_out', args, scheme=self.proving_scheme)['proof']
+
+        if self.proving_scheme == 'gm17':
+            return list(map(lambda x: int(x, 0), proof_data['a'] + proof_data['b'][0] + proof_data['b'][1] + proof_data['c']))
+        else:
+            raise NotImplementedError()
