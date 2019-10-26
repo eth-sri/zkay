bea27cb90a95aadb71bdfe568a2faa1b8e97aa4b
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-10-26 19:52:25 +0200

    implement dummy crypto backend

---
+++ zkay/transaction/crypto/dummy.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+from zkay.transaction.interface import CipherValue, RandomnessValue, PrivateKeyValue, PublicKeyValue, KeyPair
+from zkay.transaction.interface import ZkayCryptoInterface, bn256_scalar_field
+
+
+class DummyCrypto(ZkayCryptoInterface):
+
+    def _generate_or_load_key_pair(self) -> KeyPair:
+        return KeyPair(PublicKeyValue(42), PrivateKeyValue(42))
+
+    def _enc(self, plain: int, pk: PublicKeyValue) -> Tuple[CipherValue, RandomnessValue]:
+        return CipherValue((plain + pk.val) % bn256_scalar_field), RandomnessValue(1)
+
+    def _dec(self, cipher: CipherValue, sk: PrivateKeyValue) -> Tuple[int, RandomnessValue]:
+        return (cipher.val - sk.val) % bn256_scalar_field, RandomnessValue(2)
