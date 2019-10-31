16c526d07b374d2dca587701f779ae31d15a0e05
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-10-31 10:46:41 +0100

    add overflow check for arguments to zokrates prover interface

--- zkay/transaction/prover/zokrates.py
+++ zkay/transaction/prover/zokrates.py
@@ -1,12 +1,14 @@
 from typing import List
 
 from zkay.compiler.zokrates.compiler import generate_proof
-from zkay.transaction.interface import ZkayProverInterface
+from zkay.transaction.interface import ZkayProverInterface, bn128_scalar_field
 
 
 class ZokratesProver(ZkayProverInterface):
     def _generate_proof(self, verifier_dir: str, priv_values: List[int], in_vals: List[int], out_vals: List[int]) -> List[int]:
         args = list(map(int, priv_values + in_vals + out_vals))
+        for arg in args:
+            assert arg < bn128_scalar_field, "argument overflow"
         proof_data = generate_proof(f'{verifier_dir}_out', args, scheme=self.proving_scheme)['proof']
 
         if self.proving_scheme == 'gm17':
