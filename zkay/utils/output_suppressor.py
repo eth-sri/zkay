c8b82a2a452eef5fce6f640e5070ebd0d2e52b7b
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-11-02 22:57:08 +0100

    add context manager to suppress std output

---
+++ zkay/utils/output_suppressor.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from contextlib import contextmanager
+
+# Based on https://stackoverflow.com/a/17954769
+from zkay.config import debug_output_whitelist
+
+
+@contextmanager
+def output_suppressed(key: str):
+    fd = sys.stdout.fileno()
+
+    def _redirect_stdout(to):
+        sys.stdout.close()
+        os.dup2(to.fileno(), fd)
+        sys.stdout = os.fdopen(fd, 'w')
+
+    if key in debug_output_whitelist:
+        yield
+    else:
+        with os.fdopen(os.dup(fd), 'w') as old_stdout:
+            with open(os.devnull, 'w') as file:
+                _redirect_stdout(to=file)
+            try:
+                yield
+            finally:
+                _redirect_stdout(to=old_stdout)
