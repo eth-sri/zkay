e1877529d16207a87bc98a618c3712bbd6326976
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-11-13 16:56:19 +0100

    added test contract for function calls

---
+++ eval-ccs2019/examples/funccall/funccall.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.0;
+
+contract funccall {
+    final address owner;
+    uint@owner res;
+    uint pubval;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function update_pubval() public {
+        pubval = pubval + 1;
+    }
+
+    function some_comp(uint@me v1, uint@me v2) pure public returns (uint@me) {
+        return v1 + v2;
+    }
+
+    function some_comp_pub(uint@me v1, uint@me v2) pure public returns (uint) {
+        return reveal(v1 + v2, all);
+    }
+
+    function calc(uint@me v) public {
+        pubval = 0;
+        res = v + some_comp(v, v);
+        update_pubval();
+        pubval = some_comp_pub(pubval, res);
+    }
+}
