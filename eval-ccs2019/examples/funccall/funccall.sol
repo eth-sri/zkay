60be2f14dd4464a224d6f2e7b9ac2e5345cede79
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-11-14 16:33:26 +0100

    add more kinds of function calls to test contract

--- eval-ccs2019/examples/funccall/funccall.sol
+++ eval-ccs2019/examples/funccall/funccall.sol
@@ -9,14 +9,33 @@ contract funccall {
         owner = msg.sender;
     }
 
-    function update_pubval() public {
+    // Purely public function with side effects (not allowed inside circuit)
+    function update_pubval() public returns(uint) {
         pubval = pubval + 1;
+        return 1;
     }
 
-    function some_comp(uint@me v1, uint@me v2) pure public returns (uint@me) {
+    // Purely public function with static body
+    function pure_pub_func(uint val) pure public returns(uint) {
+        return val + 42;
+    }
+
+    // Purely public function with dynamic body
+    function recursive(uint val) pure public returns(uint) {
+        return val > 42 ? 0 : recursive(val+1);
+    }
+
+    // Illegal recursive function which would require verification
+    /*function recursivePriv(uint@me val) pure public returns(uint) {
+        return reveal(val > 42 ? 0 : recursive(reveal(val, all) + 1), all);
+    }*/
+
+    // Private function used in private expression (inlined)
+    function some_comp(uint@me v1, uint@me v2) pure internal returns (uint@me) {
         return v1 + v2;
     }
 
+    // Mixed function with static body
     function some_comp_pub(uint@me v1, uint@me v2) pure public returns (uint) {
         return reveal(v1 + v2, all);
     }
@@ -24,8 +43,9 @@ contract funccall {
     function calc(uint@me v) public {
         require(owner == me);
         pubval = 0;
-        res = v + some_comp(v, v);
+        uint test = recursive(23);
+        res = v + some_comp(v, v) + pure_pub_func(2);
         //update_pubval(); problem, alias analysis forgets that me == owner
-        pubval = some_comp_pub(pubval, res);
+        //pubval = some_comp_pub(pubval, res); will not work for non-purely public functions
     }
 }
