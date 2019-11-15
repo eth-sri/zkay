ab6ea1c4b7a96eb38e156f42af0d246ed14e7590
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-11-15 17:01:49 +0100

    updated funccall contract

--- eval-ccs2019/examples/funccall/funccall.sol
+++ eval-ccs2019/examples/funccall/funccall.sol
@@ -32,19 +32,32 @@ contract funccall {
 
     // Private function used in private expression (inlined)
     function some_comp(uint@me v1, uint@me v2) pure internal returns (uint@me) {
-        return v1 + v2;
+        uint x = some_comp_pub(1, 2);
+        return v1 + v2 + x;
     }
 
     // Mixed function with static body
     function some_comp_pub(uint@me v1, uint@me v2) pure public returns (uint) {
+        uint x = recursive(42) + some_comp_pub2(v1, v2);
         return reveal(v1 + v2, all);
+    }
+
+    function some_comp_pub2(uint@me v1, uint@me v2) pure public returns (uint) {
+        //uint@me hans = some_comp(v1, v2);
+        uint x = recursive(42);
+        return reveal(v1 + v2, all);
+    }
+
+    function priv_inc(uint@me val) public pure returns(uint@me) {
+        return val + 1;
     }
 
     function calc(uint@me v) public {
         require(owner == me);
         pubval = 0;
-        uint test = recursive(23);
-        res = v + some_comp(v, v) + pure_pub_func(2);
+        uint test = recursive(23) + some_comp_pub(v, v);
+        res = priv_inc(v) + 1;
+        //res = v + some_comp(v, v) + pure_pub_func(2);
         //update_pubval(); problem, alias analysis forgets that me == owner
         //pubval = some_comp_pub(pubval, res); will not work for non-purely public functions
     }
