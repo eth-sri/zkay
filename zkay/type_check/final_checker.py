50aede429603438d324f9a4db3faa2cf071d95af
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-11-27 17:11:46 +0100

    enhanced final checker (prevent multiple assignments, make sure assignment happens in all branches)

--- zkay/type_check/final_checker.py
+++ zkay/type_check/final_checker.py
@@ -1,8 +1,8 @@
-from typing import Set
+from typing import Set, Dict, Optional
 
 from zkay.type_check.type_exceptions import TypeException
 from zkay.zkay_ast.ast import StateVariableDeclaration, \
-    AssignmentStatement, IdentifierExpr, ContractDefinition
+    AssignmentStatement, IdentifierExpr, ContractDefinition, Block, ConstructorDefinition, FunctionDefinition, IfStatement
 from zkay.zkay_ast.visitor.visitor import AstVisitor
 
 
@@ -15,35 +15,52 @@ class FinalVisitor(AstVisitor):
 
     def __init__(self):
         super().__init__('node-or-children')
-        self.state_vars: Set[StateVariableDeclaration] = None
+        self.state_vars_assigned: Optional[Dict[StateVariableDeclaration, bool]] = None
 
     def visitContractDefinition(self, ast: ContractDefinition):
-        self.state_vars = set()
+        self.state_vars_assigned = {}
         for v in ast.state_variable_declarations:
-            if 'final' in v.keywords and v.expr is None:
-                self.state_vars.add(v)
+            if v.is_final and v.expr is None:
+                self.state_vars_assigned[v] = False
 
         if len(ast.constructor_definitions) > 0:
             assert (len(ast.constructor_definitions) == 1)
             c = ast.constructor_definitions[0]
-            for s in c.body:
-                if isinstance(s, AssignmentStatement):
-                    self.visit(s.rhs)
-                    if isinstance(s.lhs, IdentifierExpr):
-                        var = s.lhs.target
-                        if var in self.state_vars:
-                            self.state_vars.remove(var)
-                else:
-                    self.visit(s)
+            self.visit(c.body)
 
-        if len(self.state_vars) > 0:
-            raise TypeException("Did not set all final state variables", ast)
+        for sv, assigned in self.state_vars_assigned.items():
+            if not assigned:
+                raise TypeException("Did not set all final state variables", sv)
 
-        self.state_vars = None
+        self.state_vars_assigned = None
 
-        self.visitChildren(ast)
+    def visitFunctionDefinition(self, ast: FunctionDefinition):
+        return
+
+    def visitAssignmentStatement(self, ast: AssignmentStatement):
+        self.visit(ast.rhs)
+        if isinstance(ast.lhs, IdentifierExpr):
+            var = ast.lhs.target
+            if var in self.state_vars_assigned:
+                if self.state_vars_assigned[var]:
+                    raise TypeException("Tried to reassign final variable", ast)
+                self.state_vars_assigned[var] = True
+
+    def visitIfStatement(self, ast: IfStatement):
+        self.visit(ast.condition)
+        prev = self.state_vars_assigned.copy()
+        self.visit(ast.then_branch)
+        then_b = self.state_vars_assigned.copy()
+        self.state_vars_assigned = prev
+        if ast.else_branch is not None:
+            self.visit(ast.else_branch)
+
+        assert then_b.keys() == self.state_vars_assigned.keys()
+        for var in then_b.keys():
+            if then_b[var] != self.state_vars_assigned[var]:
+                raise TypeException("Final value is not assigned in both branches", ast)
 
     def visitIdentifierExpr(self, ast: IdentifierExpr):
-        if self.state_vars is not None:
-            if ast.target in self.state_vars:
+        if ast.is_rvalue() and self.state_vars_assigned is not None:
+            if ast.target in self.state_vars_assigned and not self.state_vars_assigned[ast.target]:
                 raise TypeException(f'{str(ast)} is reading "final" state variable before writing it', ast)
