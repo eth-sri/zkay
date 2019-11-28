897937ffa55fc8e34df78100a73ca2bdb1688e51
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-11-28 12:31:42 +0100

    requires_verification_when_external now implies can_be_external

--- zkay/zkay_ast/analysis/hybrid_function_detector.py
+++ zkay/zkay_ast/analysis/hybrid_function_detector.py
@@ -1,8 +1,6 @@
 from zkay.type_check.type_exceptions import TypeException
-from zkay.zkay_ast.ast import ReclassifyExpr, ConstructorOrFunctionDefinition, AllExpr, FunctionCallExpr, \
-    LocationExpr, BuiltinFunction, Expression
+from zkay.zkay_ast.ast import ReclassifyExpr, ConstructorOrFunctionDefinition, AllExpr, FunctionCallExpr, LocationExpr, BuiltinFunction
 from zkay.zkay_ast.visitor.function_visitor import FunctionVisitor
-from zkay.zkay_ast.visitor.visitor import AstVisitor
 
 
 def detect_hybrid_functions(ast):
@@ -20,9 +18,6 @@ def detect_hybrid_functions(ast):
     v = NonInlineableCallDetector()
     v.visit(ast)
 
-    v = InlineFunctionDetector()
-    v.visit(ast)
-
 
 class DirectHybridFunctionDetectionVisitor(FunctionVisitor):
     def visitReclassifyExpr(self, ast: ReclassifyExpr):
@@ -38,13 +33,15 @@ class DirectHybridFunctionDetectionVisitor(FunctionVisitor):
 
     def visitConstructorOrFunctionDefinition(self, ast: ConstructorOrFunctionDefinition):
         self.visit(ast.body)
-        if ast.requires_verification:
-            ast.requires_verification_if_external = True
 
         if ast.can_be_external:
-            for param in ast.parameters:
-                if param.annotated_type.is_private():
-                    ast.requires_verification_if_external = True
+            if ast.requires_verification:
+                ast.requires_verification_when_external = True
+            else:
+                for param in ast.parameters:
+                    if param.annotated_type.is_private():
+                        ast.requires_verification_when_external = True
+                        break
 
 
 class IndirectHybridFunctionDetectionVisitor(FunctionVisitor):
@@ -53,29 +50,14 @@ class IndirectHybridFunctionDetectionVisitor(FunctionVisitor):
             for fct in ast.called_functions:
                 if fct.requires_verification:
                     ast.requires_verification = True
-                    ast.requires_verification_if_external = True
+                    if ast.can_be_external:
+                        ast.requires_verification_when_external = True
                     break
 
 
 class NonInlineableCallDetector(FunctionVisitor):
     def visitFunctionCallExpr(self, ast: FunctionCallExpr):
         if isinstance(ast.func, LocationExpr):
-            if ast.func.target.requires_verification_if_external and ast.func.target.is_recursive: # TODO don't inline functions which only require external verification
+            if ast.func.target.requires_verification and ast.func.target.is_recursive:
                 raise TypeException("Non-inlineable call to recursive private function", ast.func)
         self.visitChildren(ast)
-
-
-class InlineFunctionDetector(AstVisitor):
-    def visitFunctionCallExpr(self, ast: FunctionCallExpr):
-        if isinstance(ast.func, LocationExpr):
-            if ast.func.target.requires_verification_if_external and not ast.is_private: # TODO don't inline functions which only require external verification
-                ast.contains_inlined_function = True
-                return True
-        return self.visitExpression(ast)
-
-    def visitExpression(self, ast: Expression):
-        ast.contains_inlined_function = any(self.visit(c) for c in ast.children())
-        return ast.contains_inlined_function
-
-    def visitAST(self, _):
-        return False
