47e48197b693a9bed03aa15cd328d5ad569762bb
Nick Baumann <8076953+nibau@users.noreply.github.com>
2019-12-06 12:08:55 +0100

    minimal encryption example and scenario (1 enc, 1 dec)

---
+++ zkay/examples/code/EncTest.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+contract EncTest {
+    final address owner;
+    uint@owner v;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function test(uint val) public {
+        require(owner == me);
+        v = v + val + 2;
+    }
+}
---
+++ zkay/examples/scenarios/enctest.py
@@ -0,0 +1,13 @@
+from zkay.examples.scenario import ScenarioBuilder
+a = 'a'
+sb = ScenarioBuilder('RealEncrypt', 'code/EncTest.sol').set_users(a)
+sb.set_deployment_transaction(owner=a)
+sb.add_balance_assertion(0)
+sb.add_state_assertion('v', expected_value=0, should_decrypt=True)
+sb.add_transaction('test', [42], user=a)
+sb.add_state_assertion('v', expected_value=44, should_decrypt=True)
+sb.add_balance_assertion(0)
+sb.add_transaction('test', [7], user=a)
+sb.add_state_assertion('v', expected_value=53, should_decrypt=True)
+sb.add_balance_assertion(0)
+SCENARIO = sb.build()
