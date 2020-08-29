ae09ec50ba806f4e6daae205cab46cee13494520
Nick Baumann <8076953+nibau@users.noreply.github.com>
2020-08-29 12:19:08 +0200

    prevent unsupported solc versions from being used via version override flag
    fix overridden version not correctly applied if it was not previously installed

--- zkay/config_version.py
+++ zkay/config_version.py
@@ -4,7 +4,7 @@ This module defines pinned versions and is used internally to configure the conc
 import os
 import sys
 
-from semantic_version import NpmSpec
+from semantic_version import NpmSpec, Version
 
 
 class Versions:
@@ -31,12 +31,16 @@ class Versions:
                 sys.exit(100)
         else:
             try:
+                v = Version(version)
+                if not Versions.ZKAY_SOLC_VERSION_COMPATIBILITY.match(v):
+                    raise ValueError(f'Zkay only supports solc versions satisfying {Versions.ZKAY_SOLC_VERSION_COMPATIBILITY.expression}')
                 solcx.set_solc_version(version, silent=True)
             except ValueError as e:
                 raise ValueError(f'Invalid version string {version}\n{e}')
             except SolcNotInstalled:
                 try:
                     solcx.install_solc(version)
+                    solcx.set_solc_version(version, silent=True)
                 except Exception as e:
                     print(f'ERROR: Error while trying to install solc version {version}\n{e.args}')
                     sys.exit(101)
