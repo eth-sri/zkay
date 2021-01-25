c912149e59f384edb8c8046f4ba2585c82994a7a
Roger Baumgartner <rogerb@student.ethz.ch>
2021-01-25 12:53:58 -0800

    Add test for homomorphic multiplications with signed plaintexts

---
+++ zkay/examples/scenarios/hom_mul.py
@@ -0,0 +1,13 @@
+from zkay.examples.scenario import ScenarioBuilder
+a, b = 'a', 'b'
+sb = ScenarioBuilder('HomomorphicMultiplication', 'code/HomomorphicMultiplication.zkay').set_users(a, b)
+sb.set_deployment_transaction(-3, 2, owner=a)
+sb.add_state_assertion('a', expected_value=-3, should_decrypt=True)
+sb.add_state_assertion('b', expected_value=2)
+sb.add_transaction('f', user=b)
+sb.add_state_assertion('productPosPos', expected_value=-6, should_decrypt=True)
+sb.add_state_assertion('productPosNeg', expected_value=6, should_decrypt=True)
+sb.add_state_assertion('productNegPos', expected_value=6, should_decrypt=True)
+sb.add_state_assertion('productNegNeg', expected_value=-6, should_decrypt=True)
+sb.add_balance_assertion(0)
+SCENARIO = sb.build()
