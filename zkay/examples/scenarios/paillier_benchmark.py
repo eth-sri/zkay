c568a2e6dfb52e661e41b91d6b3a9637efd0e613
Roger Baumgartner <rogerb@student.ethz.ch>
2021-02-18 20:45:25 -0800

    Add an assertion to the paillier benchmark scenario so the test doesn't fail...

--- zkay/examples/scenarios/paillier_benchmark.py
+++ zkay/examples/scenarios/paillier_benchmark.py
@@ -3,4 +3,5 @@ from zkay.examples.scenario import ScenarioBuilder
 a = 'a'
 sb = ScenarioBuilder('PaillierBenchmark', 'code/PaillierBenchmark.zkay').set_users(a)
 sb.set_deployment_transaction(owner=a)
+sb.add_balance_assertion(0)
 SCENARIO = sb.build()
