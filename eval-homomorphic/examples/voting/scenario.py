f77e036e4f3418c8ab6c992212eaa5f4dd434352
sam-steffen <42912036+sam-steffen@users.noreply.github.com>
2021-05-13 14:46:09 +0200

    voting example

---
+++ eval-homomorphic/examples/voting/scenario.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+from zkay.zkay_frontend import transaction_benchmark_ctx
+
+# Scenario
+with transaction_benchmark_ctx(sys.argv[1]) as g:
+	a_addr, b_addr, c_addr, owner_addr = g.create_dummy_accounts(4)
+
+	owner = g.deploy(user=owner_addr)
+	a = g.connect(owner.address, user=a_addr)
+	b = g.connect(owner.address, user=b_addr)
+	c = g.connect(owner.address, user=c_addr)
+
+	a.vote(0)
+	b.vote(1)
+	c.vote(1)
+	owner.declareWinner()
