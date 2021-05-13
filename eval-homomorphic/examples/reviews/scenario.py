ca6b609bb7bc7ae01ade1a24337d0d37df94a331
sam-steffen <42912036+sam-steffen@users.noreply.github.com>
2021-05-13 15:10:17 +0200

    reviews example

---
+++ eval-homomorphic/examples/reviews/scenario.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+from zkay.zkay_frontend import transaction_benchmark_ctx
+
+# Scenario
+with transaction_benchmark_ctx(sys.argv[1]) as g:
+	pc_addr, r1_addr, r2_addr, author_addr = g.create_dummy_accounts(4)
+
+	pc = g.deploy(user=pc_addr)
+	r1 = g.connect(pc.address, user=r1_addr)
+	r2 = g.connect(pc.address, user=r2_addr)
+	author = g.connect(pc.address, user=author_addr)
+
+	pc.registerReviewer(r1_addr)
+	pc.registerReviewer(r2_addr)
+	author.registerPaper(1234)
+	r1.recordReview(1234, 3)
+	r2.recordReview(1234, 2)
+	pc.decideAcceptance(author_addr)
