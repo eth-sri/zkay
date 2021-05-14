ac228a59366a287c8f23b7ab3b5d58d3ac0c12e0
sam-steffen <42912036+sam-steffen@users.noreply.github.com>
2021-05-14 16:31:42 +0200

    zether-overflow example

---
+++ eval-homomorphic/examples/zether-overflow/scenario.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+from zkay.zkay_frontend import transaction_benchmark_ctx
+
+# Scenario
+with transaction_benchmark_ctx(sys.argv[1]) as g:
+	sender_addr, receiver_addr = g.create_dummy_accounts(2)
+
+	sender = g.deploy(user=sender_addr)
+	receiver = g.connect(sender.address, user=receiver_addr)
+
+	sender.fund(wei_amount=1000)
+	sender.transfer(receiver_addr, 100)
+	receiver.into_vault(45)
+	receiver.burn(55)
+	receiver.from_vault(45)
