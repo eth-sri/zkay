1a293d4edeed8179bd311df9dc012c5fd126a991
sam-steffen <42912036+sam-steffen@users.noreply.github.com>
2021-05-27 16:51:20 +0200

    metadata for all examples

--- eval-homomorphic/extract_metadata.py
+++ eval-homomorphic/extract_metadata.py
@@ -3,16 +3,17 @@
 
 import re
 import os
+from operator import itemgetter
 file_dir = os.path.realpath(os.path.dirname(__file__))
 cwd = os.path.join(file_dir, 'examples')
 
 
 scenarios = [d for d in os.listdir(cwd) if os.path.isdir(os.path.join(cwd, d))]
-metadata = {}
+all_metadata = []
 for name in scenarios:
     contract_file_name = os.path.join(cwd, name, name + ".zkay")
     if os.path.exists(contract_file_name):
-        metadata[name] = {}
+        metadata = {}
         loc = 0
         with open(contract_file_name, "r") as f:
             for line in f:
@@ -22,11 +23,13 @@ for name in scenarios:
                 obj = re.match(r"^// META-([A-Z]+)( (.*))?$", line)
                 if obj is not None:
                     g = obj.groups()
-                    metadata[name][g[0]] = g[2]
-        metadata[name]['LOC'] = loc     # for the moment simply counts non-emtpy lines
+                    metadata[g[0]] = g[2]
+        metadata['LOC'] = loc     # for the moment simply counts non-emtpy lines
+        if "NAME" in metadata:
+            all_metadata.append(metadata)
 
 print("xxcontract,xxdescription,xxloc,xxadd,xxmult,xxmixed")
-for _, data in metadata.items():
+for data in sorted(all_metadata, key=itemgetter("NAME")):
     if "NAME" not in data:
         continue
     x_add = "\\xyes" if "ADD" in data else "\\xno"
