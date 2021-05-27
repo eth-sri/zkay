6f9792baa8760beb65c4e0c87ade836208d6d62c
sam-steffen <42912036+sam-steffen@users.noreply.github.com>
2021-05-27 10:45:33 +0200

    oblivious-transfer example

---
+++ eval-homomorphic/examples/oblivious-transfer/scenario.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+from zkay.zkay_frontend import transaction_benchmark_ctx
+
+# Scenario
+with transaction_benchmark_ctx(sys.argv[1]) as g:
+	receiver_addr, sender_addr = g.create_dummy_accounts(2)
+
+	receiver = g.deploy(user=receiver_addr)
+	sender = g.connect(receiver.address, user=sender_addr)
+
+	receiver.prepare(0, 1)
+	sender.send(24, 59)
+
+	receiver.prepare(1, 0)
+	sender.send(42, 18)
