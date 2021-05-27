46b38a70e05a99630670c2177f773de3c74a7739
sam-steffen <42912036+sam-steffen@users.noreply.github.com>
2021-05-27 10:20:30 +0200

    shared-prod example

---
+++ eval-homomorphic/examples/shared-prod/scenario.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+from zkay.zkay_frontend import transaction_benchmark_ctx
+
+# Scenario
+with transaction_benchmark_ctx(sys.argv[1]) as g:
+	owner_addr, other_addr = g.create_dummy_accounts(2)
+
+	owner = g.deploy(user=owner_addr)
+	other = g.connect(owner.address, user=other_addr)
+
+	other.foo(4)
+	owner.show_result()
